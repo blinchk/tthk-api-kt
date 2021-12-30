743592563f44398c8be09e67c3609bc55d12bd5d
Nikolas Laus <bchik3@gmail.com>
2021-12-30 14:23:11 +0200

    Add constant values to CalendarUtil.

--- src/main/kotlin/ee/bredbrains/tthkapi/util/CalendarUtil.kt
+++ src/main/kotlin/ee/bredbrains/tthkapi/util/CalendarUtil.kt
@@ -3,10 +3,15 @@ package ee.bredbrains.tthkapi.util
 import java.util.*
 
 object CalendarUtil {
+    private const val LESSONS_BEGINNING_HOURS = 8
+    private const val LESSONS_BEGINNING_MINUTES = 30
+    private const val LESSONS_BEGINNING_SECONDS = 0
+    private const val LESSONS_BEGINNING_MILLISECONDS = 0
+
     fun Calendar.setDefaults() {
-        this.set(Calendar.HOUR_OF_DAY, 8)
-        this.set(Calendar.MINUTE, 30)
-        this.set(Calendar.SECOND, 0)
-        this.set(Calendar.MILLISECOND, 0)
+        this.set(Calendar.HOUR_OF_DAY, LESSONS_BEGINNING_HOURS)
+        this.set(Calendar.MINUTE, LESSONS_BEGINNING_MINUTES)
+        this.set(Calendar.SECOND, LESSONS_BEGINNING_SECONDS)
+        this.set(Calendar.MILLISECOND, LESSONS_BEGINNING_MILLISECONDS)
     }
 }
